14a639c1f93d0759315af3c26e3923f333be3bf8
isaac_lindenman <tomisaaccohen@gmail.com>
2023-09-21 11:06:16 +0300

    fix: updated translation in home screen

--- ts-web/src/app/page.tsx
+++ ts-web/src/app/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
 import Link from 'next/link';
+import useTranslation from 'next-translate/useTranslation';
 
 import { Navigation } from '@/constants';
 
 export default function Home() {
+	const { t } = useTranslation('common');
 	return (
-		<main
-			data-testid="login-container"
-			className="h-full w-full flex items-center justify-center bg-base-200"
-		>
-			<Link href={Navigation.SignIn}>SIGN-IN</Link>
+		<main className="h-full w-full flex items-center justify-center bg-base-100">
+			<Link data-testid="sign-in-link" href={Navigation.SignIn}>
+				{t('sign-in')}
+			</Link>
 		</main>
 	);
 }
