5e2c008200dc1004dc0e54dca07f83829333097f
isaac_lindenman <tomisaaccohen@gmail.com>
2023-09-21 12:03:48 +0300

    fix: added Translation to logo

--- ts-web/src/components/logo.tsx
+++ ts-web/src/components/logo.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
+import useTranslation from 'next-translate/useTranslation';
 
 export function Logo() {
+	const { t } = useTranslation('common');
+
 	return (
-		<div className="align-baseline flex">
+		<div className="align-baseline flex" data-testid="logo-component">
 			<Image
 				priority
 				width="26"
@@ -10,7 +13,9 @@ export function Logo() {
 				src="/images/pinecone-transparent-bg.svg"
 				alt="Logo"
 			/>
-			<span className="text-2xl font-bold text-primary">BaseMind</span>
+			<span className="text-2xl font-bold text-primary">
+				{t('basemindName')}
+			</span>
 		</div>
 	);
 }
