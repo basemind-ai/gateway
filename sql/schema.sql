ec648160bd4af6ed7e6901a4f92df631e7c2422f
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-09-23 19:35:20 +0200

    feat: added prompt-request-record and prompt-config tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -53,14 +53,55 @@ CREATE TABLE application
 (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     description text NOT NULL,
-    model_parameters json NOT NULL,
-    model_type model_type NOT NULL,
-    model_vendor model_vendor NOT NULL,
     name varchar(256) NOT NULL,
-    prompt_messages json NOT NULL,
-    expected_template_variables varchar(256) [] NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now(),
     project_id uuid NOT NULL,
     FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE
+);
+
+-- prompt-config
+CREATE TABLE prompt_config
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name varchar(256) NOT NULL,
+    model_parameters json NOT NULL,
+    model_type model_type NOT NULL,
+    model_vendor model_vendor NOT NULL,
+    prompt_messages json NOT NULL,
+    template_variables varchar(256) [] NULL,
+    is_active boolean NOT NULL DEFAULT FALSE,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    application_id uuid NOT NULL,
+    FOREIGN KEY (application_id) REFERENCES application (id) ON DELETE CASCADE
+);
+
+-- prompt-request-record
+CREATE TABLE prompt_request_record
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    stream_response boolean NOT NULL DEFAULT FALSE,
+    request_tokens int NOT NULL,
+    start_time timestamptz NOT NULL,
+    finish_time timestamptz NOT NULL,
+    prompt_config_id uuid NOT NULL,
+    FOREIGN KEY (prompt_config_id) REFERENCES prompt_config (
+        id
+    ) ON DELETE CASCADE
+);
+
+-- prompt-test
+CREATE TABLE prompt_test
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name varchar(256) NOT NULL,
+    variable_values json NOT NULL,
+    response text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    prompt_request_record_id uuid NOT NULL,
+    FOREIGN KEY (prompt_request_record_id) REFERENCES prompt_request_record (
+        id
+    ) ON DELETE CASCADE
 );
