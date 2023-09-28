3d5bc4eeb2d7d23ef89784f1b17912a0a518461a
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-09-28 15:56:10 +0200

    fix: resolved gradle issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -9,9 +10,38 @@ plugins {
     alias(libs.plugins.version.catalog.update)
 }
 
+versionCatalogUpdate {
+    sortByKey.set(true)
+    pin {}
+    keep {
+        keepUnusedVersions.set(false)
+        keepUnusedLibraries.set(false)
+        keepUnusedPlugins.set(false)
+    }
+}
+
 tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
     checkForGradleUpdate = true
     checkConstraints = true
     checkBuildEnvironmentConstraints = true
-    gradleReleaseChannel = "release-candidate"
+    gradleReleaseChannel = "current"
+    reportfileName = "report"
+}
+
+/**
+ * isNonStable checks if a given version is not stable
+ */
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val nonStableKeyword = listOf("ALPHA", "BETA", "RC").any { version.uppercase().contains(it) }
+
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = !nonStableKeyword && (stableKeyword || regex.matches(version))
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
 }
