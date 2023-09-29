e0ee25a48f076558f0be3f91b1a8c46de94194ae
Dipti-1401 <deeptigupta522@gmail.com>
2023-09-29 12:04:05 +0530

    chore: add unit test

---
+++ frontend/tests/app/prompt.spec.tsx
@@ -0,0 +1,37 @@
+import locales from 'public/locales/en.json';
+import { render, screen } from 'tests/test-utils';
+
+import Prompt from '@/app/project/[projectId]/prompt/page';
+
+const promptTranslation = locales.prompt;
+describe('Prompt page tests', () => {
+	it('render Prompt testing', async () => {
+		render(<Prompt />);
+
+		const dashboardContainer = screen.getByTestId('prompt');
+		expect(dashboardContainer).toBeInTheDocument();
+	});
+
+	it('render Prompt testing heading', async () => {
+		render(<Prompt />);
+
+		const promptHeader = screen.getByText(promptTranslation.promptHeader);
+		expect(promptHeader).toBeInTheDocument();
+	});
+
+	it('render Prompt testing saved template', async () => {
+		render(<Prompt />);
+
+		const promptSavedTemplates = screen.getByText(
+			promptTranslation.promptSavedTemplates,
+		);
+		expect(promptSavedTemplates).toBeInTheDocument();
+	});
+
+	it('render Prompt testing new button', async () => {
+		render(<Prompt />);
+
+		const promptNewButton = screen.getByText(promptTranslation.promptNew);
+		expect(promptNewButton).toBeInTheDocument();
+	});
+});
