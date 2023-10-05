6e1aabd57f57d4c6e825d7cf2f22ddee0cb2ec4a
Dipti-1401 <deeptigupta522@gmail.com>
2023-10-05 18:03:54 +0530

    chore: setup daisyUI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -26,13 +26,13 @@ module.exports = {
 		require('daisyui'),
 	],
 	daisyui: {
-		themes: ['dracula'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: 'dracula', // name of one of the included themes for dark mode
-		base: true, // applies background color and foreground color for root element by default
-		styled: true, // include daisyUI colors and design decisions for all components
-		utils: true, // adds responsive and modifier utility classes
-		rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
-		prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+		themes: ['dracula'],
+		darkTheme: 'dracula',
+		base: true,
+		styled: true,
+		utils: true,
+		rtl: false,
+		prefix: '',
+		logs: true,
 	},
 };
