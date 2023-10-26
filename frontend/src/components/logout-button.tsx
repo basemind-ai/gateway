6ff7455d072dfa7b5c3462a1e6fe2842d52abe82
isaac_lindenman <tomisaaccohen@gmail.com>
2023-10-26 08:43:20 +0300

    feat: added logout-button.tsx to setting page

--- frontend/src/components/logout-button.tsx
+++ frontend/src/components/logout-button.tsx
@@ -1,15 +1,18 @@
 import { useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 
+import { useSetUser } from '@/stores/api-store';
 import { getFirebaseAuth } from '@/utils/firebase';
 
 export function LogoutButton() {
 	const t = useTranslations('common');
 	const router = useRouter();
+	const setUser = useSetUser();
 
 	async function handleLogout() {
 		const auth = await getFirebaseAuth();
 		await auth.signOut();
+		setUser(null);
 		router.replace('/');
 	}
 
@@ -19,6 +22,7 @@ export function LogoutButton() {
 				void handleLogout();
 			}}
 			data-testid="dashboard-logout-btn"
+			className="btn btn-sm btn-outline btn-accent"
 		>
 			{t('logout')}
 		</button>
