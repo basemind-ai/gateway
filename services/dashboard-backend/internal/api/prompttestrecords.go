64b87515b6b2de75d937cc638f670e76565ade26
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-11-13 17:12:02 +0100

    feat: implement prompt-test-record API

--- services/dashboard-backend/internal/api/prompttestrecords.go
+++ services/dashboard-backend/internal/api/prompttestrecords.go
@@ -1,12 +1,94 @@
 package api
 
 import (
+	"github.com/basemind-ai/monorepo/services/dashboard-backend/internal/dto"
 	"github.com/basemind-ai/monorepo/services/dashboard-backend/internal/middleware"
+	"github.com/basemind-ai/monorepo/shared/go/apierror"
+	"github.com/basemind-ai/monorepo/shared/go/db"
+	"github.com/basemind-ai/monorepo/shared/go/exc"
+	"github.com/basemind-ai/monorepo/shared/go/serialization"
 	"github.com/jackc/pgx/v5/pgtype"
+	"k8s.io/utils/ptr"
 	"net/http"
 )
 
+// handleRetrievePromptTestRecords - retrieves all prompt test records for the given application.
 func handleRetrievePromptTestRecords(w http.ResponseWriter, r *http.Request) {
 	applicationID := r.Context().Value(middleware.ApplicationIDContextKey).(pgtype.UUID)
 
+	records := exc.MustResult(db.GetQueries().RetrievePromptTestRecords(r.Context(), applicationID))
+
+	data := make([]*dto.PromptTestRecordDTO, len(records))
+
+	for i, record := range records {
+		var promptConfigID *string
+		if !record.IsTestConfig.Bool {
+			promptConfigID = ptr.To(db.UUIDToString(ptr.To(record.PromptConfigID)))
+		}
+
+		data[i] = &dto.PromptTestRecordDTO{
+			ID:                     db.UUIDToString(ptr.To(record.ID)),
+			CreatedAt:              record.CreatedAt.Time,
+			ErrorLog:               ptr.To(record.ErrorLog.String),
+			FinishTime:             record.FinishTime.Time,
+			ModelParameters:        record.ModelParameters,
+			ModelType:              record.ModelType.ModelType,
+			ModelVendor:            record.ModelVendor.ModelVendor,
+			Name:                   record.Name,
+			PromptConfigID:         promptConfigID,
+			PromptResponse:         record.Response,
+			ProviderPromptMessages: record.ProviderPromptMessages,
+			RequestTokens:          record.RequestTokens.Int32,
+			ResponseTokens:         record.ResponseTokens.Int32,
+			StartTime:              record.StartTime.Time,
+			StreamResponseLatency:  record.StreamResponseLatency.Int64,
+			UserInput:              record.VariableValues,
+		}
+	}
+
+	serialization.RenderJSONResponse(w, http.StatusOK, data)
+}
+
+// handleRetrievePromptTestRecord - retrieves a single prompt test record by its ID.
+func handleRetrievePromptTestRecord(w http.ResponseWriter, r *http.Request) {
+	testRecordID := r.Context().Value(middleware.PromptTestRecordID).(pgtype.UUID)
+
+	record, retrievalErr := db.GetQueries().RetrievePromptTestRecord(r.Context(), testRecordID)
+	if retrievalErr != nil {
+		apierror.BadRequest("test record does not exist").Render(w)
+		return
+	}
+
+	var promptConfigID *string
+	if !record.IsTestConfig.Bool {
+		promptConfigID = ptr.To(db.UUIDToString(&record.PromptConfigID))
+	}
+
+	serialization.RenderJSONResponse(w, http.StatusOK, &dto.PromptTestRecordDTO{
+		ID:                     db.UUIDToString(&record.ID),
+		CreatedAt:              record.CreatedAt.Time,
+		ErrorLog:               ptr.To(record.ErrorLog.String),
+		FinishTime:             record.FinishTime.Time,
+		ModelParameters:        record.ModelParameters,
+		ModelType:              record.ModelType.ModelType,
+		ModelVendor:            record.ModelVendor.ModelVendor,
+		Name:                   record.Name,
+		PromptConfigID:         promptConfigID,
+		PromptResponse:         record.Response,
+		ProviderPromptMessages: record.ProviderPromptMessages,
+		RequestTokens:          record.RequestTokens.Int32,
+		ResponseTokens:         record.ResponseTokens.Int32,
+		StartTime:              record.StartTime.Time,
+		StreamResponseLatency:  record.StreamResponseLatency.Int64,
+		UserInput:              record.VariableValues,
+	})
+}
+
+// handleDeletePromptTestRecord - deletes a prompt test record by its ID.
+func handleDeletePromptTestRecord(w http.ResponseWriter, r *http.Request) {
+	testRecordID := r.Context().Value(middleware.PromptTestRecordID).(pgtype.UUID)
+
+	exc.Must(db.GetQueries().DeletePromptTestRecord(r.Context(), testRecordID))
+
+	w.WriteHeader(http.StatusNoContent)
 }
---
+++ services/dashboard-backend/internal/api/prompttestrecords_test.go
@@ -0,0 +1,275 @@
+package api_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/basemind-ai/monorepo/e2e/factories"
+	"github.com/basemind-ai/monorepo/services/dashboard-backend/internal/api"
+	"github.com/basemind-ai/monorepo/services/dashboard-backend/internal/dto"
+	"github.com/basemind-ai/monorepo/shared/go/db"
+	"github.com/basemind-ai/monorepo/shared/go/db/models"
+	"github.com/basemind-ai/monorepo/shared/go/exc"
+	"github.com/basemind-ai/monorepo/shared/go/serialization"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/utils/ptr"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestPromptTestRecordsAPI(t *testing.T) {
+	project, _ := factories.CreateProject(context.TODO())
+	application, _ := factories.CreateApplication(context.TODO(), project.ID)
+	userAccount, _ := factories.CreateUserAccount(context.TODO())
+	_, _ = db.GetQueries().CreateUserProject(context.TODO(), models.CreateUserProjectParams{
+		ProjectID:  project.ID,
+		UserID:     userAccount.ID,
+		Permission: models.AccessPermissionTypeADMIN,
+	})
+
+	testClient := createTestClient(t, userAccount)
+
+	promptConfig, _ := factories.CreatePromptConfig(context.TODO(), application.ID)
+	firstPromptRequestRecord, _ := factories.CreatePromptRequestRecord(
+		context.TODO(),
+		promptConfig.ID,
+	)
+	firstPromptTestRecord, _ := db.GetQueries().
+		CreatePromptTestRecord(context.TODO(), models.CreatePromptTestRecordParams{
+			Name:                  "test 1",
+			PromptRequestRecordID: firstPromptRequestRecord.ID,
+			Response:              "i am a bot and i will take over the world",
+			VariableValues:        []byte(`{"userInput": "bar"}`),
+		})
+
+	// test config - this emulates a config that was not created by the user, but rather during a test
+
+	testPromptConfig := exc.MustResult(
+		db.GetQueries().CreatePromptConfig(context.TODO(), models.CreatePromptConfigParams{
+			Name:            "test-config-1",
+			ModelParameters: []byte("{}"),
+			ModelType:       models.ModelTypeCommand,
+			ModelVendor:     models.ModelVendorCOHERE,
+			ProviderPromptMessages: []byte(
+				`{"message": "please write a poem about topic {userInput}"}`,
+			),
+			ExpectedTemplateVariables: []string{"userInput"},
+			IsDefault:                 false,
+			ApplicationID:             application.ID,
+			IsTestConfig:              true,
+		}),
+	)
+	secondPromptRequestRecord := exc.MustResult(
+		db.GetQueries().
+			CreatePromptRequestRecord(context.TODO(), models.CreatePromptRequestRecordParams{
+				IsStreamResponse:   true,
+				RequestTokens:      5,
+				ResponseTokens:     20,
+				RequestTokensCost:  *exc.MustResult(db.StringToNumeric("10")),
+				ResponseTokensCost: *exc.MustResult(db.StringToNumeric("50")),
+				StartTime: pgtype.Timestamptz{
+					Time:  time.Now().Add(-10 * time.Second),
+					Valid: true,
+				},
+				FinishTime: pgtype.Timestamptz{
+					Time:  time.Now().Add(-9 * time.Second),
+					Valid: true,
+				},
+				StreamResponseLatency: pgtype.Int8{Int64: 1000, Valid: true},
+				PromptConfigID:        testPromptConfig.ID,
+				ErrorLog:              pgtype.Text{String: "", Valid: true},
+			}),
+	)
+	secondPromptTestRecord := exc.MustResult(
+		db.GetQueries().CreatePromptTestRecord(context.TODO(), models.CreatePromptTestRecordParams{
+			Name:                  "test 2",
+			PromptRequestRecordID: secondPromptRequestRecord.ID,
+			Response:              "roses are red, violets are blue, I will become a robot and so will you.",
+			VariableValues:        []byte(`{"userInput": "bar"}`),
+		}),
+	)
+
+	listEndpointURL := func(applicationID string) string {
+		return fmt.Sprintf(
+			"/v1%s",
+			strings.ReplaceAll(strings.ReplaceAll(
+				api.PromptTestRecordListEndpoint,
+				"{projectId}",
+				db.UUIDToString(&project.ID),
+			), "{applicationId}", applicationID),
+		)
+	}
+
+	detailEndpointURL := func(applicationID string, testRecordID string) string {
+		return fmt.Sprintf(
+			"/v1%s",
+			strings.ReplaceAll(
+				strings.ReplaceAll(strings.ReplaceAll(
+					api.PromptTestRecordDetailEndpoint,
+					"{projectId}",
+					db.UUIDToString(&project.ID),
+				), "{applicationId}", applicationID), "{promptTestRecordId}", testRecordID),
+		)
+	}
+
+	t.Run(fmt.Sprintf("GET: %s", api.PromptTestRecordListEndpoint), func(t *testing.T) {
+		t.Run("retrieves an applications prompt test records", func(t *testing.T) {
+			response, requestErr := testClient.Get(
+				context.TODO(),
+				listEndpointURL(db.UUIDToString(&application.ID)),
+			)
+			assert.NoError(t, requestErr)
+			assert.Equal(t, http.StatusOK, response.StatusCode)
+
+			data := make([]*dto.PromptTestRecordDTO, 0)
+			deserializationErr := serialization.DeserializeJSON(response.Body, &data)
+			assert.NoError(t, deserializationErr)
+
+			assert.Len(t, data, 2)
+
+			recordOne := data[0]
+			assert.Equal(t, recordOne.ID, db.UUIDToString(&firstPromptTestRecord.ID))
+			assert.Equal(t, recordOne.Name, firstPromptTestRecord.Name)
+			assert.Equal(t, recordOne.PromptConfigID, ptr.To(db.UUIDToString(&promptConfig.ID)))
+
+			recordTwo := data[1]
+			assert.Equal(t, recordTwo.ID, db.UUIDToString(&secondPromptTestRecord.ID))
+			assert.Equal(t, recordTwo.Name, secondPromptTestRecord.Name)
+			assert.Empty(t, recordTwo.PromptConfigID)
+		})
+
+		t.Run("handles an empty list of prompt test records", func(t *testing.T) {
+			newApplication, _ := factories.CreateApplication(context.TODO(), project.ID)
+
+			response, requestErr := testClient.Get(
+				context.TODO(),
+				listEndpointURL(db.UUIDToString(&newApplication.ID)),
+			)
+			assert.NoError(t, requestErr)
+			assert.Equal(t, http.StatusOK, response.StatusCode)
+
+			data := make([]*dto.PromptTestRecordDTO, 0)
+			deserializationErr := serialization.DeserializeJSON(response.Body, &data)
+			assert.NoError(t, deserializationErr)
+
+			assert.Len(t, data, 0)
+		})
+
+		t.Run(
+			"responds with status 403 FORBIDDEN if the user does not have projects access",
+			func(t *testing.T) {
+				newUser, _ := factories.CreateUserAccount(context.TODO())
+				newTestClient := createTestClient(t, newUser)
+
+				response, requestErr := newTestClient.Get(
+					context.TODO(),
+					listEndpointURL(db.UUIDToString(&application.ID)),
+				)
+				assert.NoError(t, requestErr)
+				assert.Equal(t, http.StatusForbidden, response.StatusCode)
+			},
+		)
+	})
+
+	t.Run(fmt.Sprintf("GET: %s", api.PromptTestRecordDetailEndpoint), func(t *testing.T) {
+		t.Run("retrieves a prompt test record", func(t *testing.T) {
+			response, requestErr := testClient.Get(
+				context.TODO(),
+				detailEndpointURL(
+					db.UUIDToString(&application.ID),
+					db.UUIDToString(&firstPromptTestRecord.ID),
+				),
+			)
+			assert.NoError(t, requestErr)
+			assert.Equal(t, http.StatusOK, response.StatusCode)
+
+			data := &dto.PromptTestRecordDTO{}
+			deserializationErr := serialization.DeserializeJSON(response.Body, data)
+			assert.NoError(t, deserializationErr)
+
+			assert.Equal(t, data.ID, db.UUIDToString(&firstPromptTestRecord.ID))
+			assert.Equal(t, data.Name, firstPromptTestRecord.Name)
+			assert.Equal(t, data.PromptConfigID, ptr.To(db.UUIDToString(&promptConfig.ID)))
+		})
+
+		t.Run(
+			"responds with status 403 FORBIDDEN if the user does not have projects access",
+			func(t *testing.T) {
+				newUser, _ := factories.CreateUserAccount(context.TODO())
+				newTestClient := createTestClient(t, newUser)
+
+				response, requestErr := newTestClient.Get(
+					context.TODO(),
+					detailEndpointURL(
+						db.UUIDToString(&application.ID),
+						db.UUIDToString(&firstPromptTestRecord.ID),
+					),
+				)
+				assert.NoError(t, requestErr)
+				assert.Equal(t, http.StatusForbidden, response.StatusCode)
+			},
+		)
+	})
+
+	t.Run(fmt.Sprintf("DELETE: %s", api.PromptTestRecordDetailEndpoint), func(t *testing.T) {
+		t.Run("deletes a prompt test record", func(t *testing.T) {
+			response, requestErr := testClient.Delete(
+				context.TODO(),
+				detailEndpointURL(
+					db.UUIDToString(&application.ID),
+					db.UUIDToString(&firstPromptTestRecord.ID),
+				),
+			)
+			assert.NoError(t, requestErr)
+			assert.Equal(t, http.StatusNoContent, response.StatusCode)
+
+			_, retrievalErr := db.GetQueries().
+				RetrievePromptTestRecord(context.TODO(), firstPromptTestRecord.ID)
+			assert.Error(t, retrievalErr)
+		})
+
+		t.Run(
+			"responds with status 403 FORBIDDEN if the user does not have projects access",
+			func(t *testing.T) {
+				newUser, _ := factories.CreateUserAccount(context.TODO())
+				newTestClient := createTestClient(t, newUser)
+
+				response, requestErr := newTestClient.Delete(
+					context.TODO(),
+					detailEndpointURL(
+						db.UUIDToString(&application.ID),
+						db.UUIDToString(&firstPromptTestRecord.ID),
+					),
+				)
+				assert.NoError(t, requestErr)
+				assert.Equal(t, http.StatusForbidden, response.StatusCode)
+			},
+		)
+
+		t.Run(
+			"responds with status 401 UNAUTHORIZED if the user does not have admin permission",
+			func(t *testing.T) {
+				newUser, _ := factories.CreateUserAccount(context.TODO())
+				_, _ = db.GetQueries().
+					CreateUserProject(context.TODO(), models.CreateUserProjectParams{
+						ProjectID:  project.ID,
+						UserID:     newUser.ID,
+						Permission: models.AccessPermissionTypeMEMBER,
+					})
+				newTestClient := createTestClient(t, newUser)
+
+				response, requestErr := newTestClient.Delete(
+					context.TODO(),
+					detailEndpointURL(
+						db.UUIDToString(&application.ID),
+						db.UUIDToString(&firstPromptTestRecord.ID),
+					),
+				)
+				assert.NoError(t, requestErr)
+				assert.Equal(t, http.StatusUnauthorized, response.StatusCode)
+			},
+		)
+	})
+}
