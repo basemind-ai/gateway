bba8f586670c9a3db0f6b5b42b2617b7f6230af5
isaac_lindenman <tomisaaccohen@gmail.com>
2023-11-28 11:58:31 +0200

    fix: new discord link

--- frontend/src/constants/navigation.ts
+++ frontend/src/constants/navigation.ts
@@ -14,7 +14,7 @@ export enum Navigation {
 	TOS = '/en/terms-of-service',
 	Testing = '/en/projects/:projectId/testing',
 }
-export const DISCORD_INVITE_LINK = 'https://discord.gg/Urxchkcq';
+export const DISCORD_INVITE_LINK = 'https://discord.gg/rHytT8YB';
 export const SUPPORT_EMAIL = 'support@basemind.ai';
 
 export enum PromptConfigPageTab {
