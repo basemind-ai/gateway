cbe1aecef502ada94683241cd0e3f53b5f90463c
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-12-13 20:33:51 +0100

    chore: updated layout to include metadataBase

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,11 +8,13 @@ const inter = Inter({ subsets: ['latin'] });
 const supportedLocales = ['en'];
 const metadataImage =
 	'https://firebasestorage.googleapis.com/v0/b/basemind-ai-production.appspot.com/o/public%2Flogo-with-text.png?alt=media&token=0a2ac2b6-508b-44b0-b601-758404800f10';
+
 export const metadata: Metadata = {
 	description:
 		'The all-in-one developer platform for your mobile apps AI needs. Integrate in minutes, scale to millions.',
 	keywords:
 		'BaseMind, Serverless AI, Prompt Management, GPT-4, gemini, Developer Platform for AI, Kotlin, Swift, Flutter, React Native, API',
+	metadataBase: new URL(process.env.NEXT_PUBLIC_FRONTEND_HOST!),
 	openGraph: {
 		images: [
 			{
