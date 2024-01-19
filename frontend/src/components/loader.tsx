bbcbe6a760c0c885a1b9f9ce9855ee52b000ec91
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2024-01-19 11:01:50 +0100

    chore: fixed lottie issue

--- frontend/src/components/loader.tsx
+++ frontend/src/components/loader.tsx
@@ -1,17 +1,12 @@
-'use client';
-import Lottie from 'lottie-react';
-
-import tigerShooting from '@/../public/images/tiger-shooting.json';
+import { DotLottiePlayer } from '@dotlottie/react-player';
 
 export function LottieLoader() {
 	return (
-		<div
-			className={`flex flex-col justify-center items-center m-auto max-w-xl`}
-		>
-			<Lottie
-				animationData={tigerShooting}
-				className="flex justify-center items-center aspect-square"
-				loop={true}
+		<div className="flex flex-col justify-center items-center m-auto max-w-xl">
+			<DotLottiePlayer
+				src="/images/tiger-shooting.lottie"
+				autoplay
+				loop
 			/>
 		</div>
 	);
